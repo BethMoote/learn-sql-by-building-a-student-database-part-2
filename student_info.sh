db669d6f5adf4b7aaab6cc726adb7219083d954b
Beth Moote <beth.moote@gmail.com>
2025-02-22 12:32:08 +0000

    Save progress: 1290.1

--- student_info.sh
+++ student_info.sh
@@ -7,3 +7,4 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
+
